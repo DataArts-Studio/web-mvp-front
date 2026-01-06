c621df33f04eb5bc3c5ce285a66acbea15257034
japark <67992955+JangHwanPark@users.noreply.github.com>
2026-01-06 22:51:48 +0900

    풀 리퀘스트 병합 #29
    * Feat: 테스트 스위트 상세 페이지 및 조회 로직 추가

---
+++ app/projects/[slug]/suites/[suiteId]/page.tsx
@@ -0,0 +1,8 @@
+import SuiteDetailView from '../../../../../src/view/project/suites/suite-detail-view';
+
+
+const SuiteDetailPage = () => {
+  return <SuiteDetailView />;
+};
+
+export default SuiteDetailPage;
